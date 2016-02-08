032028c2c0a1b474bdb17a9d211aa537c9764a3e
Mihailo Isakov <mihailo@protonmail.ch>
2016-02-08 15:47:44 +0100

    Disabled form posting on enter

--- order.js
+++ order.js
@@ -25,6 +25,16 @@ $(document).ready(function() {
 			//$('#info_holder_id').toggle();
 		//}, millisTillOccurence);
 	
+
+    // Disable submiting the form when pressing Enter
+    $(document).ready(function() {
+        $(window).keydown(function(event){
+            if(event.keyCode == 13) {
+                event.preventDefault();
+                return false;
+            }
+        });
+    });
 	
 	// Funkcija koja prikazuje popover sa greskom
 	$('[data-toggle="popover"]').popover();
